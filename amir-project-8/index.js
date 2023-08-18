2ce4894b88056c1438a32adbc4da52ba26b457d9
Dzmir12 <137734970+Dzmir12@users.noreply.github.com>
2023-08-18 12:54:54 -0700

    Add files via upload

---
+++ amir-project-8/index.js
@@ -0,0 +1,8 @@
+const userInput= prompt('please enter a number',10);
+let odd=0;
+for(i=0;i<userInput;i++) {
+   if(i%2==0){
+    console.log(i);
+   }
+}
+
---
+++ amir-project-9/index.js
@@ -0,0 +1,5 @@
+const firstName = prompt('please enter your first name','amir');
+const lastName = prompt('please enter your first name','abdi');
+const fullName = firstName + lastName + " your fullname's length is:"+(firstName+lastName).length;
+
+console.log(fullName);
