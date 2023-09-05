55f7119ad8040c1ea9158095e35f9cff1329a21f
Dzmir12 <137734970+Dzmir12@users.noreply.github.com>
2023-09-05 16:15:23 -0700

    Add files via upload

---
+++ amir-project-js3/index.js
@@ -0,0 +1,120 @@
+
+// tamrin 1
+
+// const userInput = prompt('Please enter a sentence?');
+
+// function removeVowels(str) {
+//     let result = '';
+//     for (let i = 0; i < str.length; i++) {
+//         const char = str[i];
+//         if (char !== 'a' && char !== 'e' && char !== 'u' && char !== 'i' && char !== 'o') {
+//             result += char;
+//         }
+//     }
+//     return result;
+// }
+
+// console.log(removeVowels(userInput));
+
+
+
+// tamrin 2
+
+// let userArr=[];
+// isgoing=true;
+// while (isgoing) {
+//  const userInput=prompt('please enter a list of name');
+//  if (userInput.trim()!='') {
+//     userArr.push(userInput);
+//  }
+//  if (userInput=='quit') {
+//     isgoing=false;
+//     userArr.pop('quit');
+//     break;
+//  }
+// }
+// console.log(userArr)
+
+// function whoLikeThis(arr) {
+
+//      if (arr=='') {
+//         return 'nobody likes this';
+//      }else if(arr.length<=1) {
+//       return `${arr} likes this`;
+//      }else if(arr.length<=2) {
+//          const lastchar=arr.pop();
+//         return `${arr} and ${lastchar} likes this`;
+//        }else{
+//         const lastchar=arr.pop();
+//         return `${arr} and ${lastchar} likes this`;
+//        }
+// }
+
+// console.log(whoLikeThis(userArr))
+
+
+// tamrin 3
+
+// let userArr = [];
+// let isGoing = true;
+
+// while (isGoing) {
+//   const userInput = prompt('Please enter a number or type "quit" to exit:');
+
+//   if (userInput === 'quit') {
+//     isGoing = false;
+//   } else {
+//     const number = parseFloat(userInput);
+    
+//     if (!isNaN(number)) {
+//       userArr.push(number);
+//     } else {
+//       alert('Invalid input. Please enter a valid number.');
+//     }
+//   }
+// }
+
+// console.log(userArr);
+
+
+// let comparisonarr = [];
+// let haveDesire= true;
+
+// while (haveDesire) {
+//   const userInput = prompt('Please enter a comparison number or type "quit" to exit:');
+
+//   if (userInput === 'quit') {
+//     haveDesire = false;
+//   } else {
+//     const number = parseFloat(userInput);
+    
+//     if (!isNaN(number)) {
+//         comparisonarr.push(number);
+//     } else {
+//       alert('Invalid input. Please enter a valid number.');
+//     }
+//   }
+// }
+
+// console.log(comparisonarr);
+
+
+// function findArrayDifference(arr1, arr2) {
+//     const uniqueElements = new Set([...arr1, ...arr2]);
+//     const difference = [];
+//     for (const element of uniqueElements) {
+//       if (!arr1.includes(element) || !arr2.includes(element)) {
+//         difference.push(element);
+//       }
+//     }
+  
+//     return difference;
+//   }
+
+// console.log(findArrayDifference(userArr,comparisonarr));
+
+
+
+
+
+
